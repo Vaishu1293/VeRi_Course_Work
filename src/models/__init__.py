dab3db99107df19b2ef6bc7038050b21e1816c5f
Vaishu1293 <clearexamseasy@gmail.com>
2023-05-08 17:09:16 +0100

    v27

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -9,7 +9,7 @@ from .resnet import (
     resnet50_fc512,
 )
 from .tvmodels import mobilenet_v3_small, vgg16
-
+from .custom_veri_model import  import custom_veri_model
 
 __model_factory = {
     # image classification models
@@ -21,7 +21,8 @@ __model_factory = {
     "resnet50_fc512": resnet50_fc512,
     "mobilenet_v3_small": mobilenet_v3_small,
     "vgg16": vgg16,
-    "alexnet": alexnet
+    "alexnet": alexnet,
+    "custom_veri_model": custom_veri_model
 }
 
 
---
+++ src/models/custom_veri_model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+
+class CustomVeRiModel(nn.Module):
+    def __init__(self, num_classes, **kwargs):
+        super(CustomVeRiModel, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+def custom_veri_model(num_classes, **kwargs):
+    model = CustomVeRiModel(
+        num_classes=num_classes,
+        **kwargs,
+    )
+    return model
